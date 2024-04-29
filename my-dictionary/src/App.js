c1a08a279b1574c5404c89fdd21dd25a900668b3
mirielimacedo <mirielimacedo@gmail.com>
2024-04-29 12:29:09 +0800

    Initial commit

---
+++ my-dictionary/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src="https://www.shecodes.io/assets/branding/logo-shecodes-3dfa60aeab8ef361842da5a2b6d46db3af1b7afafefee3dde0a9846389de754b.png"
+          className="App-logo img-fluid"
+          alt="logo"
+        ></img>
+      </header>
+    </div>
+  );
+}
+
+export default App;
